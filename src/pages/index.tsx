2e9d51c1b338c3ff2bf5d222e9e1550fc37b7ab0
ryankashi <ryanakashi@gmail.com>
2024-12-02 19:54:50 -0800

    Finished my boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { Button } from '@nextui-org/react';
 
-export default function Home() {
+function Home() {
 	return (
-		<div>
-			<div>Hello world!</div>
-			<div className="btn btn-primary">Daisyui button</div>
-			<Button>Nextui Button!</Button>
+		<div className="p-4 space-y-2">
+			<div className="bg-primary">Hello world!</div>
+			<div className="flex gap-4 items-center bg-red-500">
+				<div className="btn btn-primary">Daisyui button</div>
+				<Button>Nextui Button!</Button>
+			</div>
 		</div>
 	);
 }
+
+export { Home as default };
